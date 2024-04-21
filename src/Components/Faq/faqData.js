475c5e6adcd5cd92d9f57c68965bfe45255e0a9e
AmolShelke2 <shelkeamol897@gmail.com>
2024-04-21 17:30:33 +0530

    finalized: all questions added in faqs data

--- src/Components/Faq/faqData.js
+++ src/Components/Faq/faqData.js
@@ -251,25 +251,6 @@ export const faqsData = [
     ),
     category: "buyers",
   },
-  {
-    id: 12,
-    question: "What information do you require to approve a property?",
-    answer: (
-      <div>
-        <p className="mb-8">
-          To approve a property you’ve placed an offer on, we just need the
-          address. As you move forward with closing, we will need the following
-          information:
-        </p>
-        <ul className="mb-8">
-          <li>The general home inspection report</li>
-          <li>All seller disclosures that are provided to the buyer</li>
-          <li>Homeowners association documents (if applicable)</li>
-        </ul>
-      </div>
-    ),
-    category: "buyers",
-  },
 
   {
     id: 13,
@@ -329,5 +310,289 @@ export const faqsData = [
       </div>
     ),
     category: "investor",
+  },
+  {
+    id: 15,
+    question: "How do I get started? Do I qualify?",
+    answer: (
+      <div>
+        <p className="mb-8">
+          The process is really simple. Just answer a few questions about you
+          and your home. It’ll take just a couple of minutes.{" "}
+          <a
+            href="https://homellc.typeform.com/qualify?typeform-source=www.home.llc"
+            target="_blank"
+            rel="noreferrer"
+            className="text-black">
+            Click here
+          </a>
+        </p>
+      </div>
+    ),
+    category: "all",
+  },
+  {
+    id: 16,
+    question: "Do I need to get a mortgage?",
+    answer: (
+      <div>
+        <p>
+          Above and beyond the 20% down payment, you’ll have to manage the rest
+          80% either through a mortgage or any other means you see fit.
+        </p>
+      </div>
+    ),
+    category: "all",
+  },
+  {
+    id: 17,
+    question: "Do I have to pay monthly?",
+    answer: (
+      <div>
+        <p>
+          Home.LLC does not charge any fee or monthly payments during the tenure
+          of our partnership.
+        </p>
+      </div>
+    ),
+    category: "all",
+  },
+  {
+    id: 18,
+    question:
+      "What happens if I want to end my partnership with Home.LLC but I don’t want to sell my home?",
+    answer: (
+      <div>
+        <p>
+          To end the partnership, you simply need to notify Home.LLC and we’ll
+          get the process started. There is an option to buy us out without
+          selling your home by refinancing with your lender or by paying us off.
+        </p>
+      </div>
+    ),
+    category: "all",
+  },
+  {
+    id: 19,
+    question: "What if I want to make improvements on the home?",
+    answer: (
+      <div>
+        <p>
+          If you want to make any modifications or improvements on your home,
+          you’re free to do it at your own cost.
+        </p>
+      </div>
+    ),
+    category: "all",
+  },
+  {
+    id: 20,
+    question:
+      "What tax implications are associated with Home.LLC's down payment program?",
+    answer: (
+      <div>
+        <p className="mb-8">
+          Home.LLC is not a tax advisor and does not provide tax advice.
+        </p>
+        <p className="mb-8">
+          Home.LLC is responsible for its taxes, and you are responsible for
+          your taxes. Under current tax law, you should not need to pay taxes
+          when you receive the cash Home.LLC’s down payment program provides
+          towards your home purchase.
+        </p>
+        <p className="mb-4">
+          You may owe taxes when your partnership with the program ends (the
+          Home.LLC Option is exercised or expired, or you buy it out early
+          through a sale of your home or otherwise).
+        </p>
+        <p>Please consult your financial advisor.</p>
+      </div>
+    ),
+    category: "all",
+  },
+  {
+    id: 21,
+    question: "Can I use Home.LLC’s funds to buy an investment property?",
+    answer: (
+      <div>
+        <p>
+          Yes, we do have a separate program for investment properties which you
+          can register for.
+        </p>
+      </div>
+    ),
+    category: "all",
+  },
+  {
+    id: 22,
+    question: "What happens if I don't work with Home.LLC’s agents?",
+    answer: (
+      <div>
+        <p>
+          Home.LLC’s only goal is to make things easier for you, you are
+          flexible to work with any agent of your choice.
+        </p>
+      </div>
+    ),
+    category: "all",
+  },
+  {
+    id: 23,
+    question: "Do you have to check my credit? When does that happen?",
+    answer: (
+      <div>
+        <p className="mb-8">
+          Yes. However, this does not impact your credit score. As part of our
+          qualification process, we use what’s known as a “soft” credit pull to
+          access your credit report. It does not affect your credit score. This
+          usually happens when you submit the application.
+        </p>
+        <p>
+          Also, over the lifetime of our agreement, we will use soft credit
+          pulls to ensure that you are continuing to meet the terms of our
+          agreement. Again, this will not affect your credit score.
+        </p>
+      </div>
+    ),
+    category: "all",
+  },
+  {
+    id: 24,
+    question: "Does Home.LLC share in selling costs?",
+    answer: (
+      <div>
+        <p className="mb-8">
+          No. When you sell your home, you’ll be responsible for those costs,
+          just like you would for any other home sale.
+        </p>
+        <p>
+          However, you should know that Home.LLC’s programs do not add any extra
+          costs at the time you sell your home.
+        </p>
+      </div>
+    ),
+    category: "all",
+  },
+  {
+    id: 25,
+    question: "Is my information safe with Home.LLC?",
+    answer: (
+      <div>
+        <p>
+          Yes. We take consumer privacy and information security of our users
+          very seriously. You can review our privacy policy for more
+          information.
+        </p>
+      </div>
+    ),
+    category: "all",
+  },
+  {
+    id: 26,
+    question: "Why should I choose you?",
+    answer: (
+      <div>
+        <p className="mb-8 flex flex-col gap-4">For 3 reasons:</p>
+        <ul>
+          <li>
+            <span className="font-bold">The best talent.</span> The best prices.
+            You get access to a young & hungry talent pool from top emerging
+            markets starting at only $20/hour.
+          </li>
+          <li>
+            <span className="font-bold">Scale fast.</span> Get free
+            replacements. Our extensive scouting network will help you secure
+            the best talent, fast. Plus, get free replacements in 30 days if
+            you’re not happy with our resources.
+          </li>
+          <li>
+            <span className="font-bold">Outsource your headaches.</span> Let us
+            handle payrolls, transfer pricing, & compliance for you.
+          </li>
+        </ul>
+      </div>
+    ),
+    category: "all",
+  },
+  {
+    id: 27,
+    question: "Is my data / information secure with you?",
+    answer: (
+      <div>
+        <p>
+          We take data security seriously, be it intellectual property,
+          proprietary data, or confidential information. Our ironclad contract
+          ensures that your proprietary assets are fully protected.
+        </p>
+      </div>
+    ),
+    category: "all",
+  },
+  {
+    id: 28,
+    question: "What services do you provide?",
+    answer: (
+      <div>
+        <p>
+          We help you fulfill your proptech talent requirements. Whether you
+          need an analyst to make the optimum real estate investment, or a sales
+          specialist to help you sell your kickass product, we’re here to help.
+        </p>
+      </div>
+    ),
+    category: "all",
+  },
+  {
+    id: 29,
+    question: "$20/hour sounds too good, what’s the catch?",
+    answer: (
+      <div>
+        <p>
+          There is no catch. Our extensive scouting network identifies the
+          hungriest emerging market talent to fulfill your requirements. We can
+          help you hire individual contributors starting $20/hour, depending on
+          your need. Text us to get started.
+        </p>
+      </div>
+    ),
+    category: "all",
+  },
+  {
+    id: 30,
+    question: "I don’t know what talent I need, how can you help?",
+    answer: (
+      <div>
+        <p>
+          Just fill up our text form. We’ll help you figure out your exact
+          requirements, and the fastest way to satisfy them.
+        </p>
+      </div>
+    ),
+    category: "all",
+  },
+  {
+    id: 31,
+    question: "I want to begin with a single resource, is that okay?",
+    answer: (
+      <div>
+        <p>
+          Yes. You can start small and scale up later as per your requirement.
+        </p>
+      </div>
+    ),
+    category: "all",
+  },
+  {
+    id: 32,
+    question: "What if I don’t like the talents you provide?",
+    answer: (
+      <div>
+        <p>
+          If you’re not satisfied with our resources, we will help you secure a
+          free replacement within 30 days.
+        </p>
+      </div>
+    ),
+    category: "all",
   },
 ];
