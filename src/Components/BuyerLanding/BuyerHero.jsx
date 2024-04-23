84e7df6eeef52510535dd6955fc1af68ba8bab7d
AmolShelke2 <shelkeamol897@gmail.com>
2024-04-23 23:50:55 +0530

    minor changes

--- src/Components/BuyerLanding/BuyerHero.jsx
+++ src/Components/BuyerLanding/BuyerHero.jsx
@@ -19,14 +19,14 @@ const BuyerHero = () => {
           {/* hero buttons */}
           <div className="sm:max-w-[710px] h-auto sm:h-[60px] flex flex-row gap-5 sm:gap-8 mt-[80px] px-[34px] sm:px-0 mb-[240px] sm:mb-0">
             <button
-              className="rounded-[50px] h-[32px] w-[171px] sm:h-auto sm:w-[310px] text-white  py-[4px]
-            sm:py-0 px-[14px] text-center text-[14px] leading-[22px] sm:text-[22px] sm:leading-[32px] capitalize bg-[#ED5272]">
-              check if you qualify
-            </button>
-            <button
-              className="border border-white rounded-[50px] h-[32px] w-[171px] sm:h-auto sm:w-[310px] text-white py-[4px]
+              className="border border-white rounded-[50px] h-[32px] w-[171px] sm:h-auto sm:w-[310px] text-white  py-[4px]
             sm:py-0 px-[14px] text-center text-[14px] leading-[22px] sm:text-[22px] sm:leading-[32px] capitalize">
               Schedule a call
+            </button>
+            <button
+              className="rounded-[50px] h-[32px] w-[171px] sm:h-auto sm:w-[310px] text-white py-[4px]
+            sm:py-0 px-[14px] text-center text-[14px] leading-[22px] sm:text-[22px] sm:leading-[32px] capitalize  bg-[#ED5272]">
+              check if you qualify
             </button>
           </div>
         </div>
